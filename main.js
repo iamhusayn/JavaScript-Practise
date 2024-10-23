8113831c7cc825e85d7fa44eb7a2f99ab747f343
Husayn <khaybabz@gmail.com>
2024-10-23 11:48:40 +0100

    Laernt and practised some basic JS

---
+++ main.js
@@ -0,0 +1,116 @@
+// console.log("Welcome to JavaScript")
+// console.log("You are now learning JavaScript")
+
+// let age = 25
+// console.log(age)
+
+// const salary = 20000
+// console.log(salary)
+
+// let sum = 0
+// sum = 8
+// console.log(sum)
+
+// const isSchool = true
+// const processType = false
+
+// let boat = null
+
+// const person = {
+//     firstName: "Husayn",
+//     lastName: "Alawiye",
+//     age: 33
+// }
+
+// console.log(person.firstName)
+
+// const evenNumbers = [2, 4, 6, 8, 10]
+// console.log(evenNumbers[3])
+
+ 
+// let x = 10
+// let y = 5
+// console.log(x + y)
+// console.log(x)
+
+// // console.log(x !== y)
+
+// const realEvenNumber = x = 6 || 8 > y
+// console.log(realEvenNumber)
+
+// const isValid = false
+// console.log(!isValid)
+
+// const apporvedUser = 60 - 18 !== 18 ? 'Age Approved' : 'Wait till you are above 18'
+// console.log(apporvedUser)
+
+// console.log(10 + 15)
+
+// const breakFast = 100
+
+// if (breakFast > "Bread") {
+//     console.log("We are having bread for breakfast")
+// } else if (breakFast < "Bread") {
+//     console.log("We are not eating")
+// } else {
+//     console.log("We are going to work without eating")
+// }
+
+
+// const lunch = 100
+
+// switch (lunch) {
+//     case 'Amala':
+//         console.log("We are having Amala for lunch")
+//         break
+//     case 'Beans and Bread':
+//         console.log("We are having Beans and Bread for lunch")
+//         break
+//     case 'Kuli Kuli and Garri':
+//         console.log("We are having Kuli Kuli and Garri for lunch")
+//     default:
+//         console.log("No food for lunch")
+// }
+
+
+// for (let dele = 1; dele <= 6; dele++) {
+//     console.log('Dele\'s Data ' + dele)
+// }
+
+
+// let dele = 1 
+// while (dele <= 6) {
+//     console.log('Dele\'s Data ' + dele)
+//     dele++
+// }
+
+// let dele = 1 
+// do {
+//     console.log('Dele\'s Data ' + dele)
+//     dele++
+// } while (dele <= 6)
+
+// function greet(classMates) {
+//     console.log(classMates + ', Welcome to JavaScript class.')
+// }
+// greet('Bunmi')
+// greet('Tunde')
+// greet('Tosin')
+
+// const a = 10
+// const b = 5
+// const sumValue = (a, b) => a + b
+// const sum = sumValue(a, b)
+// function combine(a, b) {
+//    return a + b
+// }
+// console.log(sum)
+
+
+
+
+
+
+
+
+
