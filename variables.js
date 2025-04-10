1426dab2795aed3f222b9e3cf145c7943289eec0
Husayn <khaybabz@gmail.com>
2025-04-11 00:37:51 +0100

    revisited variables

---
+++ variables.js
@@ -0,0 +1,36 @@
+// myName = "Chris";
+// function logName() {
+//   console.log(myName);
+// }
+// console.log(myName);
+
+// variable declaration
+// let myName = "Chris";
+// myName = "Bob";
+// console.log(myName);
+
+//VARIABLE TYPES
+//Arrays
+let myNameArray = ["Chris", "Bob", "Jim"];
+let myNumberArray = [10, 15, 40];
+console.log(myNameArray[0]);
+console.log(myNumberArray[0]);
+
+//Objects
+let dog = { name: "Spot", breed: "Dalmatian" };
+console.log(dog.name);
+console.log(dog.breed);
+
+let count = 1; //count is a variable and has been declared
+count = 2; //count is reassigned a new value
+// const count = 1;
+// count = 2;
+
+const bird = { species: "Kestrel" }; //bird is a constant and has been declared
+console.log(bird.species);
+bird.species = "Striated Caracara"; //bird is reassigned a new value
+console.log(bird.species);
+
+let myName;
+myName = "Husayn";
+let myAge = 30;
