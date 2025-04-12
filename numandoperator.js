9918071da48f13ab9272d2769df44eecba5c4926
Husayn <khaybabz@gmail.com>
2025-04-12 16:23:14 +0100

    more on numbers and operators

---
+++ numandoperator.js
@@ -0,0 +1,30 @@
+let num1 = 48;
+let num2 = 17;
+let num3 = 36;
+let num4 = 9;
+
+const adding = num1 + num2;
+const subtracting = num4 - num3;
+
+finalResult = adding * subtracting;
+evenOddResult = finalResult % 2 == 0 ? "Even" : "Odd";
+
+console.log(evenOddResult);
+
+// Statement 1: The elephant weighs less than the mouse
+const eleWeight = 1000;
+const mouseWeight = 2;
+weightComparison = eleWeight < mouseWeight;
+console.log(weightComparison);
+
+// Statement 2: The Ostrich is taller than the duck
+const ostrichHeight = 2;
+const duckHeight = 0.3;
+heightComparison = ostrichHeight > duckHeight;
+console.log(heightComparison);
+
+// Statement 3: The two passwords match
+const pwd1 = "stromboli";
+const pwd2 = "stROmBoLi";
+pwdMatch = pwd1 === pwd2;
+console.log(pwdMatch);
